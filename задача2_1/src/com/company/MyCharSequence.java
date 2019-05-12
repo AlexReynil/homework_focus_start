26a0e15d2a10640144a1000073e81f281c9a41a8
AlexReynil <50107793+AlexReynil@users.noreply.github.com>
2019-05-12 15:24:00 +0700

    Add files via upload
    Домашнее задание. Полуянова Александра

---
+++ задача2_1/src/com/company/MyCharSequence.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class MyCharSequence implements CharSequence {
+    public String line;
+    MyCharSequence(String s)
+    {
+        line=s;
+    }
+    @Override
+    public int length() {
+        return line.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return line.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return line.subSequence(start, end);
+    }
+}
